734181a7d99363f6372956c2cfe65ca4e5c8766d
jjwallz11 <johnnywallz86@gmail.com>
2025-09-03 09:51:15 -0500

    update for old safari

--- front11/vite.config.ts
+++ front11/vite.config.ts
@@ -1,25 +1,33 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import legacy from "@vitejs/plugin-legacy";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, resolve(__dirname, '..'), '');
+  const env = loadEnv(mode, resolve(__dirname, ".."), "");
 
   return {
-    base: '/static/',
-    plugins: [react()],
+    base: "/static/",
+    plugins: [
+      react(),
+      legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+    ],
     define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL),
+      "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
+        env.VITE_API_BASE_URL
+      ),
     },
     server: {
       open: true,
     },
     build: {
-      target: 'es2015',
-    }
+      target: "es2015",
+    },
   };
 });
