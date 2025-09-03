85a4e7ba0f49d38aef2a24d1e0e080f4cc61812c
jjwallz11 <johnnywallz86@gmail.com>
2025-09-03 13:08:49 -0500

    a lot kind of

--- front11/src/main.tsx
+++ front11/src/main.tsx
@@ -6,7 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { ModalProvider } from "./context/Modal";
 import { AuthProvider } from "./context/Auth";
-// import './index.css';
+import './index.css';
 
 if (
   import.meta.env.MODE === "production" &&
